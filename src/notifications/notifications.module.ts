b40d9494ec0c442a17453f6a98791b15db8a5c76
cl216 <colin.lawlor216@gmail.com>
2026-06-01 20:59:45 +0100

    fixing push notifications

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -5,14 +5,12 @@ import { DeviceToken } from './entities/device-token.entity';
 import { NotificationsService } from './notifications.service';
 import { NotificationsController } from './notifications.controller';
 import { PushNotificationsService } from './push-notifications.service';
-import { BookingRemindersService } from './booking-reminders.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Notification, DeviceToken])],
 providers: [
   NotificationsService,
   PushNotificationsService,
-  BookingRemindersService,
 ],
   controllers: [NotificationsController],
   exports: [NotificationsService, PushNotificationsService],
